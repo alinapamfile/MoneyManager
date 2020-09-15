307a621f49ad147ab2ce6186a9317ff421d7a35c
alinapamfile <alina.pamfile@yahoo.com>
2020-09-15 20:48:25 +0300

    password encrypting functionality

---
+++ src/main/java/utils/PasswordEncrypting.java
@@ -0,0 +1,39 @@
+package utils;
+
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
+import java.util.Base64;
+
+public class PasswordEncrypting {
+    private static final int ITERATIONS = 65536;
+    private static final int KEY_LENGTH = 512;
+    private static final String ALGORITHM = "PBKDF2WithHmacSHA512";
+
+    public static String encryptPassword(String password) {
+        KeySpec spec = new PBEKeySpec(password.toCharArray(), generateSalt(), ITERATIONS, KEY_LENGTH);
+
+        try {
+            SecretKeyFactory factory = SecretKeyFactory.getInstance(ALGORITHM);
+            byte[] hash = factory.generateSecret(spec).getEncoded();
+            return Base64.getEncoder().encodeToString(hash);
+        } catch (NoSuchAlgorithmException e) {
+            System.err.println("NoSuchAlgorithmException in encryptPassword()");
+            return null;
+        } catch (InvalidKeySpecException e) {
+            System.err.println("InvalidKeySpecException in encryptPassword()");
+            return null;
+        }
+
+    }
+
+    private static byte[] generateSalt() {
+        SecureRandom secureRandom = new SecureRandom();
+        byte[] salt = new byte[16];
+        secureRandom.nextBytes(salt);
+        return salt;
+    }
+}
