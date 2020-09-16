519d2024a1cd6919d04d155f632079d6501ba672
alinapamfile <alina.pamfile@yahoo.com>
2020-09-16 13:32:25 +0300

    add register method

--- src/main/java/utils/UserInteraction.java
+++ src/main/java/utils/UserInteraction.java
@@ -1,17 +1,19 @@
 package utils;
 
-import models.User;
+import org.bson.Document;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class UserInteraction {
     private static final Scanner scanner = new Scanner(System.in);
-    private static final User user = null;
+    private static Document user;
 
     private UserInteraction() {}
 
     public static void start() {
         System.out.println("Welcome to MoneyManager!\n");
+        sleep(1);
         authentication();
     }
 
@@ -20,43 +22,73 @@ public class UserInteraction {
         System.out.println("1. Create a new account.\n2. Log into your account.\n");
 
         do {
-            int option = scanner.nextInt();
-            switch(option) {
-                case 1 -> register();
-                case 2 -> login();
+            try {
+                int option = scanner.nextInt();
+                switch(option) {
+                    case 1 : register(); break;
+                    case 2 : login(); break;
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("\nTry again.\n");
             }
         } while(true);
     }
 
-    //TODO
     public static void register() {
+        System.out.println("Enter your firstname:");
+        String firstName = scanner.next();
+
+        System.out.println("Enter your lastname:");
+        String lastName = scanner.next();
+
+        String email;
+        do {
+            System.out.println("Enter your email:");
+            email = scanner.next();
+        } while(!ValidInput.isEmailValid(email));
+
+        String password;
+        do {
+            System.out.println("Enter a password:");
+            password = scanner.next();
+        } while(password == null);
+
+        boolean isMatching;
+        do {
+            System.out.println("Re-enter the password:");
+            isMatching = ValidInput.isPasswordMatching(scanner.next(), password);
+        } while(!isMatching);
+
+        password = PasswordEncrypting.encryptPassword(password);
+
+        user = DatabaseWork.addUser(firstName, lastName, email, password);
+        System.out.printf("\nWelcome, %s!\n", firstName);
+        run();
     }
 
     //TODO
     public static void login() {
-
+        System.exit(0);
     }
 
     //TODO
     public static void run() {
         while (true) {
             System.out.println("Please enter a number to choose an option:");
-            System.out.println("""
-                    1. Check all accounts balances.
-                    2. Check account balance.
-                    3. Check total balance.
-                    4. Logout.
-                    """
+            System.out.println("1. Check all accounts balances.\n" +
+                    "2. Check account balance.\n" +
+                    "3. Check total balance.\n" +
+                    "4. Logout.\n"
             );
 
             //temporary functionality
             do {
                 int option = scanner.nextInt();
                 switch(option) {
-                    case 1 -> System.out.println("option 1");
-                    case 2 -> System.out.println("option 2");
-                    case 3 -> System.out.println("option 3");
-                    case 4 -> exit();
+                    case 1 : System.out.println("option 1"); break;
+                    case 2 : System.out.println("option 2"); break;
+                    case 3 : System.out.println("option 3"); break;
+                    case 4 : exit();
                 }
             } while(true);
         }
