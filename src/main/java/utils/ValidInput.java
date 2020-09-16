87cbb3198e744e641f5f10a471f55db5b7ccc6c9
alinapamfile <alina.pamfile@yahoo.com>
2020-09-16 13:26:55 +0300

    add methods to validate input

---
+++ src/main/java/utils/ValidInput.java
@@ -0,0 +1,23 @@
+package utils;
+
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+
+public class ValidInput {
+
+    private ValidInput() {}
+
+    public static boolean isEmailValid(String email) {
+        try {
+            InternetAddress emailAddress = new InternetAddress(email);
+            emailAddress.validate();
+            return true;
+        } catch (AddressException e) {
+            return false;
+        }
+    }
+
+    public static boolean isPasswordMatching(String pass1, String pass2) {
+        return pass1.equals(pass2);
+    }
+}
