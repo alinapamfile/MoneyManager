d5edc93289505e75e7b7ec4dd3bc91c709f261d8
alinapamfile <alina.pamfile@yahoo.com>
2020-09-18 12:54:44 +0300

    set level for mongo driver

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,5 +1,9 @@
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Application {
     public static void main(String[] args) {
+        Logger.getLogger("org.mongodb.driver").setLevel(Level.WARNING);
         UserInteraction.start();
     }
 }
