2d75e576675923457a8c9adae1246543767cf1a2
alinapamfile <alina.pamfile@yahoo.com>
2020-09-18 12:16:21 +0300

    add necessary methods for user's operations

--- src/main/java/database/DatabaseWork.java
+++ src/main/java/database/DatabaseWork.java
@@ -1,29 +1,49 @@
-package utils;
+package database;
 
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
 
-
 public class DatabaseWork {
     private final static MongoClient mongo = DatabaseConnection.connect();
     private final static MongoDatabase db = mongo.getDatabase("money-manager");
+    private static String userEmail;
 
     private DatabaseWork() {}
 
-    public static Document addUser(String firstname, String lastname, String email, String password) {
+    //TODO: check if user exists already in the database
+    public static boolean addUser(String firstname, String lastname, String email, String password) {
         Document user = new Document("firstName", firstname)
                 .append("lastName", lastname)
                 .append("email", email)
                 .append("password", password)
                 .append("accounts", null);
         db.getCollection("users").insertOne(user);
+        userEmail = email;
 
-        return user;
+        return true;
     }
 
     //TODO
-    public static boolean deleteUser(Document user) {
+    public static boolean deleteUser() {
         return false;
     }
+
+    //TODO
+    public static void accountsBalance() {}
+
+    //TODO
+    public static void totalBalance() {}
+
+    //TODO
+    public static boolean registerExpense(String accountName, double amount) { return false; }
+
+    //TODO
+    public static boolean registerDeposit(String accountName, double amount) { return false; }
+
+    //TODO
+    public static boolean addBankAccount(Document bankAccount) { return false; }
+
+    //TODO
+    public static boolean deleteBankAccount(String accountName) { return false; }
 }
